545ea07c729181eecea3d7e710b93ac9d8dc876a
Ayshik Palit <84969513+AyshikPalit@users.noreply.github.com>
2022-01-12 17:18:54 +0530

    Add files via upload

---
+++ src/vaccine/management/system/VaccineManagementSystem.java
@@ -0,0 +1,12 @@
+package vaccine.management.system;
+/**
+ *
+ * @author Ayshik
+ */
+public class VaccineManagementSystem {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
